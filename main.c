37fb2e25fabbe1536aa81dcece9aca0132aba067
contact@microbuilder.eu <contact@microbuilder.eu@b9f98fe6-c6c4-aedc-3368-fb237f2c7aa8>
2010-05-05 22:13:15 +0000

    Cleaned up main.c.  Makefile needs to be resolved for Vista 7 nonsense with 'echo' including quotes in output

--- main.c
+++ main.c
@@ -46,7 +46,6 @@
 #include "core/uart/uart.h"
 #include "core/systick/systick.h"
 #include "core/timer32/timer32.h"
-#include "core/pmu/pmu.h"
 
 #ifdef CFG_USBHID
 #include "core/usbhid-rom/usbhid.h"
@@ -56,26 +55,21 @@
 #include "drivers/eeprom/mcp24aa/mcp24aa.h"
 #endif
 
-#ifdef CFG_LM75B
-#include "drivers/sensors/lm75b/lm75b.h"
-#endif
-
-#ifdef CFG_CHIBI
-#include "drivers/chibi/chb.h"
-#include "drivers/chibi/chb_drvr.h"
-static chb_rx_data_t rx_data;
-#endif
-
-
 int main (void)
 {
+  // Setup the cpu and core clock
   cpuInit();
-  systickInit(CFG_SYSTICK_DELAY_MS);
+
+  // Initialise the systick timer with a 1ms delay
+  systickInit(CFG_SYSTICK_DELAY_MS * 1);
+
+  // Initialise UART with the default baud rate (set in projectconfig.h)
+  // (Required since printf is redirected to UART)
   uartInit(CFG_UART_BAUDRATE);
 
   printf("CPU Initialised (72MHz).......\r\n");
 
-  // Set LED as output and turn off
+  // Set LED pin as output and turn off
   gpioSetDir(CFG_LED_PORT, CFG_LED_PIN, 1);
   gpioSetValue(CFG_LED_PORT, CFG_LED_PIN, 1);
 
@@ -90,91 +84,29 @@ int main (void)
   usbHIDInit();
   #endif
 
+  // Initialise EEPROM
   #ifdef CFG_I2CEEPROM
   printf("Initialising I2C EEPROM.......\r\n");
   mcp24aaInit();
-  uint8_t buffer[1] = { 0x00 };
-  mcp24aaWriteByte(0x0000, 0xEE);
-  mcp24aaReadByte(0x0000, buffer);
+  // uint8_t buffer[1] = { 0x00 };
+  // mcp24aaWriteByte(0x0000, 0xEE);
+  // mcp24aaReadByte(0x0000, buffer);
   #endif
-
-  #ifdef CFG_LM75B
-  int32_t temperature = 0;
-  lm75bInit();
-  #endif
-
-  #ifdef CFG_CHIBI
-  printf("Initialising Chibi (868MHz)...\r\n");
-  pcb_t *pcb = chb_get_pcb();
-  chb_init();
-  uint32_t i = 0;
-  char buf[11];
-  //  // Set address if necessary
-  //  uint8_t addr_ieee[8] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 };
-  //  chb_set_ieee_addr(addr_ieee);
-  //  uint16_t addr_short = 0x1234;
-  //  chb_set_short_addr(addr_short);
-  #endif
-
-//  // Wait 10 seconds before entering deep sleep
-//  timer32Delay(0, TIMER32_DELAY_1S * 10);
-//  pmuInit();
-//  // Enter deep sleep and wakeup after 5 seconds
-//  uint32_t pmuRegVal = SCB_PDSLEEPCFG_IRCOUT_PD |
-//                SCB_PDSLEEPCFG_IRC_PD |
-//                SCB_PDSLEEPCFG_FLASH_PD |
-//                SCB_PDSLEEPCFG_USBPLL_PD |
-//                SCB_PDSLEEPCFG_SYSPLL_PD |
-//                SCB_PDSLEEPCFG_SYSOSC_PD |
-//                SCB_PDSLEEPCFG_ADC_PD |
-//                SCB_PDSLEEPCFG_BOD_PD;
-//  pmuDeepSleep(pmuRegVal, 5);
 
   while (1)
   {
-    #ifdef CFG_CHIBI
-      #ifdef CFG_CHIBI_TRANSMITTER
-        i++;
-        sprintf(buf,"%ld",i);
-        gpioSetValue (2, 10, 0);
-        chb_write(0xFFFF, (uint8_t *)buf, 11);
-        gpioSetValue (2, 10, 1);
-        timer32Delay(0, TIMER32_DELAY_1S);
-      #endif
-      #ifdef CFG_CHIBI_RECEIVER
-        if (pcb->data_rcv)
-        {
-          // Enable LED (set low)
-          gpioSetValue (CFG_LED_PORT, CFG_LED_PIN, 0);
-          // get the length of the data
-          rx_data.len = chb_read(&rx_data);
-          // Output message to UART
-          printf("Message received from node %02X: %s, len=%d, rssi=%02X.\n", rx_data.src_addr, rx_data.data, rx_data.len, pcb->ed);
-          // Disable LED (set high)
-          gpioSetValue (CFG_LED_PORT, CFG_LED_PIN, 1);
-        }
-      #endif
-    #else
-      // Blink LED every second
-      timer32Delay(0, TIMER32_DELAY_1S * 3);
-      if (gpioGetValue(CFG_LED_PORT, CFG_LED_PIN))
-      {
-        // Enable LED (set low)
-        gpioSetValue (CFG_LED_PORT, CFG_LED_PIN, 0);
-      }
-      else
-      {
-        // Disable LED (set high)
-        gpioSetValue (CFG_LED_PORT, CFG_LED_PIN, 1);
-      }
-      #ifdef CFG_LM75B
-        lm75bGetTemperature(&temperature);
-        // Multiply value by 125 for fixed-point math (0.125°C per unit)
-        temperature *= 125;
-        // Use modulus operator to display decimal value
-        printf("Current Temperature: %ld.%ld C\r\n", temperature / 1000, temperature % 1000);
-      #endif
-    #endif
+    // Blink LED every second
+    timer32Delay(0, TIMER32_DELAY_1S * 1);
+    if (gpioGetValue(CFG_LED_PORT, CFG_LED_PIN))
+    {
+      // Enable LED (set low)
+      gpioSetValue (CFG_LED_PORT, CFG_LED_PIN, 0);
+    }
+    else
+    {
+      // Disable LED (set high)
+      gpioSetValue (CFG_LED_PORT, CFG_LED_PIN, 1);
+    }
   }
 }
 
